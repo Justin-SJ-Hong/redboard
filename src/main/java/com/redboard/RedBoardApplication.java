368077f76a49957fadcd11ddcd8621ab2953b143
Justin-SJ-Hong <madwolves98@gmail.com>
2024-09-26 21:21:27 +0900

    first commit

---
+++ src/main/java/com/redboard/RedBoardApplication.java
@@ -0,0 +1,11 @@
+package com.redboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedBoardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RedBoardApplication.class, args);
+    }
+}
